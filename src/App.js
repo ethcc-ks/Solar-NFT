4126f161f9fb68069a6fa4557a3910545d946387
neeeekitos <nikita.a.terekhov@gmail.com>
2021-07-24 11:32:39 +0200

    ui setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import * as THREE from "three";
+import { MTLLoader, OBJLoader } from "three-obj-mtl-loader";
+import OrbitControls from "three-orbitcontrols";
+class ThreeScene extends Component {
+  componentDidMount() {
+    const width = this.mount.clientWidth;
+    const height = this.mount.clientHeight;
+    this.scene = new THREE.Scene();
+    //Add Renderer
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setClearColor("#263238");
+    this.renderer.setSize(width, height);
+    this.mount.appendChild(this.renderer.domElement);
+    //add Camera
+    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    this.camera.position.z = 8;
+    this.camera.position.y = 5;
+    //Camera Controls
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    //LIGHTS
+    var lights = [];
+    lights[0] = new THREE.PointLight(0x304ffe, 1, 0);
+    lights[1] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[2] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[0].position.set(0, 200, 0);
+    lights[1].position.set(100, 200, 100);
+    lights[2].position.set(-100, -200, -100);
+    this.scene.add(lights[0]);
+    this.scene.add(lights[1]);
+    this.scene.add(lights[2]);
+    //ADD Your 3D Models here
+    const cubeGeometry = new THREE.SphereGeometry(5);
+    const material = new THREE.MeshBasicMaterial({
+      color: '#6ab056',
+      wireframe: true
+    });
+    this.cubeMesh = new THREE.Mesh(cubeGeometry, material);
+    this.scene.add(this.cubeMesh);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    const cubeGeometryNext = new THREE.SphereGeometry(2);
+    const materialNext = new THREE.MeshBasicMaterial({
+      color: '#6ah016',
+      wireframe: true
+    });
+    this.cubeMesh = new THREE.Mesh(cubeGeometry, material);
+    this.scene.add(this.cubeMesh);
+
+
+    this.renderScene();
+    //start animation
+    this.start();
+  }
+  start = () => {
+    if (!this.frameId) {
+      this.frameId = requestAnimationFrame(this.animate);}
+  };
+  stop = () => {
+    cancelAnimationFrame(this.frameId);
+  };
+  animate = () => {
+//Animate Models Here
+//ReDraw Scene with Camera and Scene Object
+    this.renderScene();
+    this.frameId = window.requestAnimationFrame(this.animate);
+  };
+  renderScene = () => {
+    if (this.renderer) this.renderer.render(this.scene, this.camera);
+  };
+
+  render() {
+    return (<div
+        style={{ width: "800px", height: "800px" }}
+        ref={mount => { this.mount = mount}}
+    />)
+  }}
+export default ThreeScene;
