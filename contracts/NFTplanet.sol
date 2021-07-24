2d83b53750ba1ae4b3505920a041a25b844d96ac
dannpr <dannba0@gmail.com>
2021-07-24 12:45:58 +0200

    planet erc721 draft

---
+++ contracts/NFTplanet.sol
@@ -0,0 +1,48 @@
+// contracts/NFTplanet.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract PlanetNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    struct Planet {
+
+    }
+
+    constructor() ERC721("PlanetNFT", "PNFT") {}
+
+    function mintPlanet(address player, string memory tokenURI) externals public returns (uint256 tokenID)
+    {
+        _tokenIds.increment();
+
+        uint256 newPlanetId = _tokenIds.current();
+
+        _mint(player, newPlanetId);
+
+        _setTokenURI(newPlanetId, tokenURI);
+
+        return newPlanetId;
+    }
+
+
+    function getParam() externals public returns (uint256 r, uint256 p, uint256 a)
+    {
+        
+         r = 21;
+         p = ;
+         a = ;
+
+    }
+
+    function updatePosition (address player, uint256 _tokenID) externals public returns (uint256 r, uint256 p, uint256 a){
+    {
+
+    }
+
+}
+
+
---
+++ contracts/PlanetReact.sol
@@ -0,0 +1,5 @@
+// contracts/PlanetReact.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "contract/"
