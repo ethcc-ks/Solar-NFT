5fe8b62a80d57087e265baa8f85a425466448c56
Aymeric Noel <aym.noel@gmail.com>
2021-07-25 12:38:34 +0200

    new builds of smart contract with deployed on rinkeby

--- migrations/3_planets.js
+++
@@ -1,7 +0,0 @@
-const NFTPlanet = artifacts.require("./NFTPlanet.sol");
-//const PlanetERC20 = artifacts.require("PlanetERC20");
-
-module.exports = function (deployer) {
-    deployer.deploy(NFTPlanet);
-}
-
