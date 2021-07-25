09b75d43b5a685c3385b3b8953810db4bb8e9fb4
Kesar <67405604+SoloSaber@users.noreply.github.com>
2021-07-25 16:59:21 +0200

    add nft

--- src/Component/LendingPopup.js
+++ src/Component/LendingPopup.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 import { Button, Form, Modal } from "react-bootstrap";
+import BlockchainContext from "../context/BlockchainContext";
 
 class LendingPopup extends Component {
 
@@ -8,11 +9,19 @@ class LendingPopup extends Component {
         super(props);
 
         this.state = {
-            amountToLend:0,
+            contract: null,
+            nft: null,
         }
 
         this.handleSubmission=this.handleSubmission.bind(this);
         this.handleLendingAmount=this.handleLendingAmount.bind(this)
+    }
+
+    async componentDidMount ()  {
+        this.setState({ contract: this.context.instance });
+        this.setState({ accounts: await this.context.accountsPromise }); 
+
+        
     }
 
     handleSubmission =  (e) => {
@@ -24,6 +33,23 @@ class LendingPopup extends Component {
         this.setState({amountToLend: e.target.value})
     }
 
+    async displayNFT(nftContract, tokenId, planetId) {
+        const addedNFT = await this.state.contract.methods.addNFT(nftContract, tokenId, planetId)
+      .send({ from: this.state.accounts[0], value: 0.002 * 10 ** 18 })
+      .then(res => {
+        console.log(res.events.Transfer.returnValues.tokenId)
+        const IDPlanet = res.events.Transfer.returnValues.tokenId;
+        console.log('Success', res);
+        alert('You have successfully created a new NFT! ID : ' + IDPlanet)
+
+        let radius = this.getRandomLogInt(5, 5);
+        this.createSphere(radius, IDPlanet);
+
+        return IDPlanet;
+      })
+      .catch(err => console.log(err));
+    }
+
     render() {
         return (
             <div className='popup' style={{position: 'absolute', top: 0, margin: 'auto', right: 0}}>
@@ -32,17 +58,25 @@ class LendingPopup extends Component {
                         <Modal.Header>
                             <Modal.Title>You are on the planet {this.props.planetID}.</Modal.Title>
                         </Modal.Header>
-                        <Modal.Body>
-
-                        </Modal.Body>
-                        <div>
-                            <Button variant="secondary" style={{ margin: "10px" }} onClick={this.props.closePopup}>Close</Button>
-                            
+                        <div className="mb-3">
+                        <Modal.Title>Add NFT to your planet</Modal.Title>
+                            <input className="form-control" type="text" id="formName" onChange={this.changeName} placeholder='NFT Contract'/>
+                            <input className="form-control" type="text" id="formName" onChange={this.changeName} placeholder='Token ID'/>
                         </div>
+                        <div>
+                            <Button variant="primary" style={{ margin: "10px" }} onClick={this.handleSubmission}>Submit</Button>
+                            <Button variant="secondary" style={{ margin: "10px" }} onClick={this.props.closePopup}>Close</Button>
+                        </div>
+                        <Modal.Body>
+                            <Modal.Title>{this.state.nft}</Modal.Title>
+                        </Modal.Body>
                     </Modal.Dialog>
                 </div>
             </div>
         );
     }
 }
+
+LendingPopup.contextType = BlockchainContext;
+
 export default LendingPopup;
