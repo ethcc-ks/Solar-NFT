8177830560d2537abbcd6d32b98efd3e5638e843
Kesar <67405604+SoloSaber@users.noreply.github.com>
2021-07-25 12:27:40 +0200

    mint planet

--- src/Component/LendingPopup.js
+++ src/Component/LendingPopup.js
@@ -24,21 +24,13 @@ class LendingPopup extends Component {
         this.setState({amountToLend: e.target.value})
     }
 
-    async fetchNFT (contractAddress, tokenID) {
-        const req = await axios.get(`https://rinkeby-api.opensea.io/api/v1/asset/${contractAddress}/${tokenID}`)
-            .then(function(response) {
-                return response
-            })
-        return {contract: req.data.asset_contract.address, image: req.data.asset_contract.image_url, name: req.data.name, owner: req.data.owner.address, tokenID: req.data.token_id};
-    }
-
     render() {
         return (
             <div className='popup' style={{position: 'absolute', top: 0, margin: 'auto'}}>
                 <div className='popup_inner'>
                     <Modal.Dialog centered>
                         <Modal.Header>
-                            <Modal.Title>You are on the ... planet</Modal.Title>
+                            <Modal.Title>You are on the {this.props.planetID} planet</Modal.Title>
                         </Modal.Header>
                         <Form>
                             <Form.Group controlId="formBasicText">
